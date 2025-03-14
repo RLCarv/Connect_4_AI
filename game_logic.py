cd0d3d1d073d3ada23fc003f9abdf5c2bb40c468
Rodrigo Carvalho <143852680+RLCarv@users.noreply.github.com>
2025-03-14 16:39:51 +0000

    Update 1 game logic

--- game_logic.py
+++ game_logic.py
@@ -3,3 +3,12 @@ NUM_COL = 7
 EMPTY = "-"
 PLAYER_1_PIECE = "X"
 PLAYER_2_PIECE = "O"
+
+class game:
+    def __init__(self):
+        self.first = PLAYER_1_PIECE #quem começa
+        self.turn = 0 #contador do turno
+        pass
+        
+    def drawBoard(self):
+        pass
